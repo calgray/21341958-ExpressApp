658d4b604cbcee3bb770b99a3c9c5b70d75f5ec8
Callan Gray <cal.j.gray@gmail.com>
2016-11-11 11:29:02 +0800

    plotting each city now

--- public/scripts/myscript.js
+++ public/scripts/myscript.js
@@ -6,26 +6,39 @@
 //dc - D3 warpper that can perform data binding to D3 graphs
 //ndx - 2-way data binding pipeline
 
-queue().defer(d3.json, "/api/data").await(makeGraphs);
+queue().defer(d3.json, "/api/data?hashtag=australia").await(makeAustraliaGraph);
+queue().defer(d3.json, "/api/data?hashtag=perth").await(makePerthGraph);
+queue().defer(d3.json, "/api/data?hashtag=sydney").await(makeSydneyGraph);
+queue().defer(d3.json, "/api/data?hashtag=melbourne").await(makeMelbourneGraph);
 
-function makeGraphs(error, apiData) {
+function makeAustraliaGraph(error, apiData) {
+  makeGraph(apiData, "australia")
+}
 
+function makePerthGraph(error, apiData) {
+  makeGraph(apiData, "perth")
+}
+
+function makeSydneyGraph(error, apiData) {
+  makeGraph(apiData, "sydney")
+}
+
+function makeMelbourneGraph(error, apiData) {
+  makeGraph(apiData, "melbourne")
+}
+
+function makeGraph(apiData, tag) {
   var dataset = apiData;
-
   var ndx = crossfilter(dataset);
 
-  var hashtag = "australia";
   var scoreDimension = ndx.dimension(function(d) { return d.score; });
   var popularityDimension = ndx.dimension(function(d) { return d.popularity; });
-
   var summedPopularity = scoreDimension.group().reduceSum(function(d) { return d.total; });
-
-
 
   //var all = ndx.groupAll();
   //var totalPopularity = popularity.group().reduceSum(function(d) { return d.total_popularity });
 
-  var popularitySpread = dc.barChart("#popularity-spread");
+  var popularitySpread = dc.barChart("#popularity-spread-" + tag);
 
   popularitySpread
       .height(220)
@@ -41,8 +54,6 @@ function makeGraphs(error, apiData) {
       .renderVerticalGridLines(true)
       .ordering(function(d){return d.value;})
       .yAxis().tickFormat(d3.format("s"));
-
-  popularitySpread.render();
 
   dc.renderAll();
 };
